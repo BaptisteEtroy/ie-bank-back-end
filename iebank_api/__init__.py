a81dad4bbe62d177f1131bbb9c0749838537088d
Baptiste Etroy <baptiste.etroy@gmail.com>
2024-10-13 18:42:27 +0200

    final changes to make it work

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:8080", "http://127.0.0.1:8080"])
+CORS(app, origins=["http://localhost:8080", "http://192.168.1.89:8080"])
 
 load_dotenv()
 
